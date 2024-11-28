c0826c40ddfa99d3b497af7ca17b830847e2cc02
Sa-10u <117238638+Sa-10u@users.noreply.github.com>
2024-11-28 11:10:20 +0900

    イージング修正　毎回０リセットするようになった。いろいろなイージングタイプを選べるようになった。 GameEditorの一部スペルミス修正

--- Engine/DirectX/Easing.cpp
+++ Engine/DirectX/Easing.cpp
@@ -4,11 +4,15 @@
 #include<numbers>
 #include<cmath>
 #include<utility>
+#include"Direct3D.h"
 
-float Easing::GetValue(float v0, float v1, float ratio, Easing::TYPE type)
+float Easing::GetValue(float v0, float v1, float pile, Easing::TYPE type)
 {
-	auto func = GetCalcFunction(type);
-	return func(v0, v1, ratio);
+	auto func = Easing::GetCalcFunction(type);
+
+	double ratio = func(pile);
+
+	return (v0*ratio) + ((1-ratio) * v1);
 }
 
 Easing::Easing(float v0, float v1, float addRatio) :val0_(v0), val1_(v1), ratio_(addRatio), pile_(.0f), isStop(false)
@@ -27,79 +31,66 @@ float Easing::GetValue(Easing::TYPE type)
 	if(!isStop)	pile_ += ratio_;
 
 	Check(.0f, 1.0f, pile_);
-	return func(val0_, val1_, pile_);
+
+	double ratio = func(pile_);
+
+	return (ratio * val0_) + ((1 - ratio) * val1_);
 }
 
-
-float Easing::CalcStraight(float v0, float v1, float ratio)
+double Easing::CalcStraight(double t)
 {
-	Check(v0, v1, ratio);
-
-	float invRatio = 1 - ratio;
-
-	return ((ratio * v0) + (invRatio * v1));
+	return t;
 }
 
 std::string Easing::GetEnumName(Easing::TYPE t)
 {
+
 	switch (t)
 	{
-	case TYPE::STRAIGHT:return "Straight";
+    case TYPE::STRAIGHT: return "Straight";
+    case TYPE::IN_SINE: return "InSine";
+    case TYPE::OUT_SINE: return "OutSine";
+    case TYPE::IN_OUT_SINE: return "InOutSine";
+    case TYPE::IN_QUAD: return "InQuad";
+    case TYPE::OUT_QUAD: return "OutQuad";
+    case TYPE::IN_OUT_QUAD: return "InOutQuad";
+    case TYPE::IN_CUBIC: return "InCubic";
+    case TYPE::OUT_CUBIC: return "OutCubic";
+    case TYPE::IN_QUART: return "InQuart";
+    case TYPE::OUT_QUART: return "OutQuart";
+    case TYPE::IN_OUT_QUART: return "InOutQuart";
+    case TYPE::IN_QUINT: return "InQuint";
+    case TYPE::OUT_QUINT: return "OutQuint";
+    case TYPE::IN_OUT_QUINT: return "InOutQuint";
+    case TYPE::IN_EXPO: return "InExpo";
+    case TYPE::OUT_EXPO: return "OutExpo";
+    case TYPE::IN_OUT_EXPO: return "InOutExpo";
+    case TYPE::IN_CIRC: return "InCirc";
+    case TYPE::OUT_CIRC: return "OutCirc";
+    case TYPE::IN_OUT_CIRC: return "InOutCirc";
+    case TYPE::IN_BACK: return "InBack";
+    case TYPE::OUT_BACK: return "OutBack";
+    case TYPE::IN_OUT_BACK: return "InOutBack";
+    case TYPE::IN_ELASTIC: return "InElastic";
+    case TYPE::OUT_ELASTIC: return "OutElastic";
+    case TYPE::IN_OUT_ELASTIC: return "InOutElastic";
+    case TYPE::IN_BOUNCE: return "InBounce";
+    case TYPE::OUT_BOUNCE: return "OutBounce";
+    case TYPE::IN_OUT_BOUNCE: return "InOutBounce";
 
-	case TYPE::SIN:		return "Sin";
-
-	case TYPE::COS:		return "Cos";
-
-	case TYPE::EaseIO:	return "EaseIO";
-
-	default: return "None";
+	default: return "Error";
 	}
 }
 
-float Easing::CalsEaseIO(float v0, float v1, float ratio)
-{
-	Check(v0, v1, ratio);
-
-	ratio = .5f + (std::sin(std::numbers::pi * (ratio - 0.5))) * .5f;
-	float invRatio = 1 - ratio;
-
-	return ((ratio * v0) + (invRatio * v1));
-}
-
-float Easing::CalcSin(float v0, float v1, float ratio)
-{
-	Check(v0, v1, ratio);
-
-	ratio = std::sin((std::numbers::pi / 2) * ratio);
-	float invRatio = 1 - ratio;
-
-	return ((ratio * v0) + (invRatio * v1));
-}
-
-float Easing::CalcCos(float v0, float v1, float ratio)
-{
-	Check(v0, v1, ratio);
-
-	ratio =1- std::cos((std::numbers::pi / 2) * ratio);
-	float invRatio = 1 - ratio;
-
-	return ((ratio * v0) + (invRatio * v1));
-}
-
-float(*Easing::GetCalcFunction(Easing::TYPE type))(float, float, float)
+std::function<double(double)>(Easing::GetCalcFunction(Easing::TYPE type))
 {
 	switch (type)
 	{
 	case Easing::TYPE::STRAIGHT:	
-		return Easing::CalcStraight;
-	case Easing::TYPE::SIN:
-		return Easing::CalcSin;
-	case Easing::TYPE::COS:
-		return Easing::CalcCos;
-	case Easing::TYPE::EaseIO:
-		return Easing::CalsEaseIO;
+		return std::function<double(double)>(Easing::CalcStraight);
+		
+    default:
+        return (Direct3D::EaseFunc.at(GetEnumName(type)));
 
-	default:
-		return Easing::CalcStraight;
 	}
 }
