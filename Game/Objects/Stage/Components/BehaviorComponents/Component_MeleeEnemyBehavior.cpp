f5335b92eca5bbb373809be5fddb9eeefba8b7c4
Nayet <n4shoya@icloud.com>
2024-12-05 16:44:05 +0900

    MeleeEnemyでやることをざっくりコメントで書いた

--- Game/Objects/Stage/Components/BehaviorComponents/Component_MeleeEnemyBehavior.cpp
+++ Game/Objects/Stage/Components/BehaviorComponents/Component_MeleeEnemyBehavior.cpp
@@ -1,7 +1,8 @@
 #include "Component_MeleeEnemyBehavior.h"
 
 Component_MeleeEnemyBehavior::Component_MeleeEnemyBehavior(string _name, StageObject* _holder, Component* _parent):
-	Component(_holder, _name, MeleeEnemyBehavior, _parent)
+	Component(_holder, _name, MeleeEnemyBehavior, _parent),
+	enemyState_(E_IDLE)
 {
 }
 
@@ -11,6 +12,14 @@ void Component_MeleeEnemyBehavior::Initialize()
 
 void Component_MeleeEnemyBehavior::Update()
 {
+
+	switch (enemyState_)
+	{
+	case E_IDLE:	Idle();		break;
+	case E_MOVE:	Move();		break;
+	case E_ATTACK:	Attack();	break;
+	case E_DEAD:	Dead();		break;
+	}
 }
 
 void Component_MeleeEnemyBehavior::Release()
@@ -27,4 +36,33 @@ void Component_MeleeEnemyBehavior::Load(json& _loadObj)
 
 void Component_MeleeEnemyBehavior::DrawData()
 {
+}
+
+void Component_MeleeEnemyBehavior::Idle()
+{
+	// PlayerBehaviorを持ってるオブジェクトを探す
+	// 属性でもいいかも？
+	// 見つけたらMoveに変更
+}
+
+void Component_MeleeEnemyBehavior::Move()
+{
+	// 見つけたPlayerBehaviorを持ってるオブジェクトの方向に対して進む
+	// プレイヤーと一定の距離まで近づいたらAttackに変更
+
+	// 一定距離離れたらIdleに変更
+}
+
+void Component_MeleeEnemyBehavior::Attack()
+{
+	// 前方方向扇形に攻撃判定を出す
+	// 攻撃判定が当たったらプレイヤーのHPを減らす
+
+	// 攻撃判定が当たらなかったらMoveに変更
+	// プレイヤーが一定距離以上離れたらidleに変更
+}
+
+void Component_MeleeEnemyBehavior::Dead()
+{
+	// 死亡アニメ
 }
--- Game/Objects/Stage/Components/BehaviorComponents/Component_MeleeEnemyBehavior.h
+++ Game/Objects/Stage/Components/BehaviorComponents/Component_MeleeEnemyBehavior.h
@@ -4,6 +4,14 @@ class Component_MeleeEnemyBehavior :
 	public Component
 {
 private:
+	enum EnemyState
+	{
+		E_IDLE = 1,
+		E_MOVE,
+		E_ATTACK,
+		E_DEAD,
+		E_MAX
+	}enemyState_;
 
 public:
 	Component_MeleeEnemyBehavior(string _name, StageObject* _holder, Component* _parent);
@@ -13,5 +21,11 @@ public:
 	void Save(json& _saveObj) override;
 	void Load(json& _loadObj) override;
 	void DrawData() override;
+
+	void Idle();
+	void Move();
+	void Attack();
+	void Dead();
+
 };
 
