137aca78da632c00155b34330bc33b2a618111b2
Nayet <n4shoya@icloud.com>
2024-12-29 18:15:50 +0900

    マージテスト

---
+++ Game/Otheres/EnemyGenerator.h
@@ -0,0 +1,54 @@
+#pragma once
+#include"Generator.h"
+
+class Collider;
+
+class EnemyGenerator:public Generator
+{
+public:
+	EnemyGenerator(XMFLOAT3 pos);
+	~EnemyGenerator();
+
+	/// <summary>
+	/// void* Ç…ÇÕStage*Çì¸ÇÍÇƒÇÀÅB
+	/// </summary>
+	/// <param name="fileDir"></param>
+	/// <param name="parent"></param>
+	void Generate(void* parent) override;
+	void* Pop() override;
+	bool isEmpty() override;
+	void Draw() override;
+
+
+	virtual void Save(json& saveObj, int index);
+	virtual void Load(json& loadObj, int index);
+
+	struct Information
+	{
+		Generator::RANGE_TYPE RangeType;
+
+		float radiusX;
+		float radiusY;
+		float radiusZ;
+		uint16_t enemyNum;
+		string enemyName;
+		string modelPath;
+
+		void Save(string& jsonName , bool isFinish);
+		void Load(string& jsonName , bool isInit);
+
+		Information();
+	}information_;
+
+protected:
+
+	void BoxGenerate();
+	void SphereGenerate();
+
+	std::vector<StageObject*> enemies_;
+	Stage* parent_;
+	Collider* collider_;
+	
+	Generator::RANGE_TYPE prevRangeType_;
+};
+
