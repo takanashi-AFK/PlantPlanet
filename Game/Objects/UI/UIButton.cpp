04bff143081893dba0f5464830bf66f0f8276fb5
Sa-10u <117238638+Sa-10u@users.noreply.github.com>
2025-01-21 11:10:37 +0900

    ボタンの表示、　UIButtonのバグ修正

--- Game/Objects/UI/UIButton.cpp
+++ Game/Objects/UI/UIButton.cpp
@@ -211,6 +211,63 @@ void UIButton::DrawData()
 				}
 			}
 		}
+
+
+		if (ImGui::SmallButton("Image")) {
+			//現在のカレントディレクトリを覚えておく
+			char defaultCurrentDir[MAX_PATH];
+			GetCurrentDirectory(MAX_PATH, defaultCurrentDir);
+
+			// 追加するオブジェクトのモデルファイルパスを設定
+			{
+				// 「ファイルを開く」ダイアログの設定用構造体を設定
+				OPENFILENAME ofn; {
+					TCHAR szFile[MAX_PATH] = {}; // ファイル名を格納するバッファ
+					ZeroMemory(&ofn, sizeof(ofn)); // 構造体の初期化
+					ofn.lStructSize = sizeof(ofn); // 構造体のサイズ
+					ofn.lpstrFile = szFile; // ファイル名を格納するバッファ
+					ofn.lpstrFile[0] = '\0'; // 初期化
+					ofn.nMaxFile = sizeof(szFile); // ファイル名バッファのサイズ
+					ofn.lpstrFilter = TEXT("PNGファイル(*.png)\0*.png\0すべてのファイル(*.*)\0*.*\0"); // フィルター（FBXファイルのみ表示）
+					ofn.nFilterIndex = 1; // 初期選択するフィルター
+					ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST; // フラグ（ファイルが存在すること、パスが存在することを確認）
+					ofn.lpstrInitialDir = TEXT("."); // カレントディレクトリを初期選択位置として設定
+				}
+
+				// ファイルを選択するダイアログの表示
+				if (GetOpenFileName(&ofn) == TRUE) {
+					// ファイルパスを取得
+					imageFilePath_ = ofn.lpstrFile;
+
+					// カレントディレクトリからの相対パスを取得
+					imageFilePath_ = FileManager::GetAssetsRelativePath(imageFilePath_);
+
+					// 文字列内の"\\"を"/"に置換
+					FileManager::ReplaceBackslashes(imageFilePath_);
+
+					// ディレクトリを戻す
+					SetCurrentDirectory(defaultCurrentDir);
+
+					// 画像を読み込む
+					imageHandle_ = Image::Load(imageFilePath_);
+				}
+				else {
+					return;
+				}
+			}
+		}
+
+		if (ImGui::Button("Clear"))
+		{
+			imageFilePath_ = {};
+			selectImageFilePath_ = {};
+			defaultImageFilePath_ = {};
+			imageHandle_ = -1;
+			selectImageHandle_ = -1;
+			defaultImageHandle_ = -1;
+			isChangeImage_ = false;
+		}
+
 		ImGui::TreePop();
 	}
 
