2394b7ebbe00d4c925b036e37e81574a549df516
Nayet <n4shoya@icloud.com>
2025-01-27 14:20:11 +0900

    予期しないEOFがなんちゃらってエラーを排除 /**/のコメントが悪さをしていた

--- Game/Objects/UI/UIInventory.cpp
+++ Game/Objects/UI/UIInventory.cpp
@@ -65,7 +65,6 @@ namespace UIInventory {
 		int x, y;
 		itemPanel_->GetButtonIndex(&x, &y);
 
-		/*入力処理*/
 		{
 			if (Input::IsPadButtonDown(XINPUT_GAMEPAD_DPAD_LEFT)) {
 				// 最初の入力かつ、選択できるボタンがある場合
